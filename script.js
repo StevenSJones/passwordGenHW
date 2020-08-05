be9c7a88ccc9c30c4d198e92d22e941c776f50e8
SS Jones <SSJones.code@gmail.com>
2020-08-04 20:57:24 -0700

    I connected the style.css file as well as added the css styling.

---
+++ script.js
@@ -0,0 +1,96 @@
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var minChar = 8;
+var maxChar = 128;
+
+// User variables
+var numOfChar = 0;
+var specChar = false;
+var nums = false;
+var upperCase = false;
+var lowerCase = false;
+
+const specialCharacters = [
+    "@",
+    "%",
+    "+",
+    "\\",
+    "/",
+    "'",
+    "!",
+    "#",
+    "$",
+    "^",
+    "?",
+    ":",
+    ",",
+    ")",
+    "(",
+    "}",
+    "{",
+    "]",
+    "[",
+    "~",
+    "-",
+    "_",
+    "."
+  ];
+  
+  // constant Array of lowercase chars to be included in password declared globally
+  const lowerCasedCharacters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ];
+  
+  // constant Array of uppercase chars to be included in password declared globally
+  const upperCasedCharacters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
+  ];
